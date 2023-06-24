ac4070eaedf07b035fc7600df6e458818a69cd5e
Sanmitha <sanmithasadanand@gmail.com>
2023-06-25 02:54:58 +0530

    Save new user data in MongoDB

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,22 +1,51 @@
 const router = require("express").Router();
+
 const admin = require("../config/firebase.config");
 
+const user = require("../models/user");
+
 router.get("/login", async (req, res) => {
-    if (!req.headers.authorization) {
+    if (!req.headers.authorization){
       return res.status(500).send({ message: "Invalid Token" });
     }
+
     const token = req.headers.authorization.split(" ")[1];
     try{
         const decodeValue = await admin.auth().verifyIdToken(token);
         if (!decodeValue) {
-            return res.status(500).json({ message: "Un Authorize" });
+            return res.status(505).json({ message: "UnAuthorized" });
         }else{
-            return res.status(200).json(decodeValue)
+            // checking user email already exists or not
+            const userExists = await user.findOne({ "user_id": decodeValue.user_id });
+            if (!userExists) {
+            newUserData(decodeValue, req, res);
+            } else {
+            return res.send("Need to update")
+            }
         }
 
     } catch(error){
         return res.status(505).json({message : error})
     }
 })
+
+const newUserData = async (decodeValue, req, res) => {
+    const newUser = new user({
+      name: decodeValue.name,
+      email: decodeValue.email,
+      imageURL: decodeValue.picture,
+      user_id: decodeValue.user_id,
+      email_verfied: decodeValue.email_verified,
+      role: "member",
+      auth_time: decodeValue.auth_time,
+    })
+
+    try {
+      const savedUser = await newUser.save();
+      res.status(200).send({ user: savedUser });
+    } catch (error) {
+      res.status(400).send({ success: false, msg: error });
+    }
+  }
 
 module.exports = router;
