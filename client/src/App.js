5b833d92eb7f3aa0951f93949546e5024014a4f8
Sanmitha <sanmithasadanand@gmail.com>
2023-06-24 19:12:46 +0530

    Add sign in with popup

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import { Route, Routes } from "react-router-dom";
 import { Home, Login } from "./components";
 
 const App = () =>{
+
+  
     return(
         <div className="w-screen h-screen bg-primary flex justify-center 
         items-center">
--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,14 +1,31 @@
-import React from 'react'
+import React from 'react';
 
-import {FcGoogle} from 'react-icons/fc'
+import { app } from '../config/firebase.config';
+import {GoogleAuthProvider, getAuth, signInWithPopup} from 'firebase/auth';
+
+import {FcGoogle} from 'react-icons/fc';
+import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
+
+  const firebaseAuth = getAuth(app);
+  const provider = new GoogleAuthProvider();
+
+  const navigate = useNavigate();
+
+  const loginWithGoogle = async () => {
+    await signInWithPopup(firebaseAuth, provider).then((userCred) => {
+      console.log(userCred)
+    })
+  }
   return (
     <div className='relative w-screen h-screen'>
       <div className='absolute inset-0 bg-darkOverlay flex items-center justify-center p-4'>
         <div className='w-full md:w-375 p-4 bg-lightOverlay shadow-2xl rounded-md backdrop-blur-md flex flex-col justify-center'>
           <div className='flex items-center justify-center gap-2 px-4 py-2 rounded-md bg-cardOverlay
-          cursor-pointer hover:bg-card hover:shadow-md duration-100 ease-in-out transition-all'>
+          cursor-pointer hover:bg-card hover:shadow-md duration-100 ease-in-out transition-all'
+          onClick={loginWithGoogle}
+          >
             <FcGoogle  className='text-xl'/>
             Sign In with Google
           </div>
