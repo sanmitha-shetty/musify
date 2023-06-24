5025f7fbbdded80df8a92b992ed1ce19f0e88c59
Sanmitha <sanmithasadanand@gmail.com>
2023-06-24 14:33:47 +0530

    Create login page

---
+++ client/src/components/Login.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+import {FcGoogle} from 'react-icons/fc'
+
+const Login = () => {
+  return (
+    <div className='relative w-screen h-screen'>
+      <div className='absolute inset-0 bg-darkOverlay flex items-center justify-center p-4'>
+        <div className='w-full md:w-375 p-4 bg-lightOverlay shadow-2xl rounded-md backdrop-blur-md flex flex-col justify-center'>
+          <div className='flex items-center justify-center gap-2 px-4 py-2 rounded-md bg-cardOverlay
+          cursor-pointer hover:bg-card hover:shadow-md duration-100 ease-in-out transition-all'>
+            <FcGoogle  className='text-xl'/>
+            Sign In with Google
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Login
