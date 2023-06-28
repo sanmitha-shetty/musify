c919087969e5ade8306174c45947961584e73c60
Sanmitha <sanmithasadanand@gmail.com>
2023-06-28 16:53:03 +0530

    Create update song request

--- server/routes/songs.js
+++ server/routes/songs.js
@@ -37,5 +37,50 @@ router.get("/getOne/:id", async (req, res) => {
     }
   });
 
+//Find All Songs
+router.get("/getAll", async (req, res) =>{
+    const options ={ 
+        sort: ({
+            createdAt: 1 
+        }),
+    };
+    //options didnt work
+    const data = await song.find().sort( { createdAt: 1 } )
+    if (data) {
+       return res.status(200).send({ success: true, song: data });
+      } else {
+        return res.status(400).send({ success: false, msg: "No Data Found" });
+      }
+
+});
+
+//Update Song
+router.put("/update/:id", async (req, res) =>{
+
+    const filter = { _id: req.params.id };
+    const options = {
+      upsert: true,
+      new: true,
+    };
+  
+    try {
+     const result = await song.findOneAndUpdate(filter, 
+      {
+        name: req.body.name,
+        imageURL: req.body.imageURL,
+        songUrl: req.body.songUrl,
+        album: req.body.album,
+        artist: req.body.artist,
+        language: req.body.language,
+        category: req.body.category,
+      }, 
+      options);
+  
+      return res.status(200).send({success: true, data: result});
+    } catch (error) {
+      return res.status(400).send({ success: false, msg: error });
+    }
+  });
+  
 
 module.exports = router
