88d72f17aaa0914a9c91ee610001ff3ffd26b0f2
Sanmitha <sanmithasadanand@gmail.com>
2023-06-28 22:02:01 +0530

    Display count on dashboard

--- client/src/components/DashboardHome.jsx
+++ client/src/components/DashboardHome.jsx
@@ -2,6 +2,9 @@ import React, { useEffect } from 'react'
 import { useStateValue } from '../context/StateProvider';
 import { getAllAlbums, getAllArtists, getAllSongs, getAllUsers } from '../api';
 import { actionType } from '../context/reducer';
+import { FaUsers } from 'react-icons/fa';
+import { GiLoveSong, GiMusicalNotes } from "react-icons/gi";
+import { RiUserStarFill } from "react-icons/ri";
 
 export const DashboardCard = ({ icon, name, count }) => {
   //const bg_color = bgColors[parseInt(Math.random() * bgColors.length)];
@@ -60,10 +63,13 @@ const DashboardHome = () => {
   }, [])
   return (
     <div className='w-full p-6 flex items-center justify-evenly flex-wrap'>
-      <DashboardCard />
-      <DashboardCard />
-      <DashboardCard />
-      <DashboardCard />
+      <DashboardCard icon={ <FaUsers className="text-3xl text-textColor"/>} name={"Users"} count={ allUsers?.length> 0 ? allUsers?.length : 0 } />
+
+      <DashboardCard icon={ <GiLoveSong className="text-3xl text-textColor"/>} name={"Songs"} count={ allSongs?.length> 0 ? allSongs?.length : 0 } />
+
+      <DashboardCard icon={ <RiUserStarFill className="text-3xl text-textColor"/>} name={"Artists"} count={ allArtists?.length> 0 ? allArtists?.length : 0 } />
+      
+      <DashboardCard icon={ <GiMusicalNotes className="text-3xl text-textColor"/>} name={"Albums"} count={ allAlbums?.length> 0 ? allAlbums?.length : 0 } />
     </div>
   )
 }
