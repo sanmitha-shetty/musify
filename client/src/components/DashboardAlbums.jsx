eb357274ca626c46b92c396c79f47ed0e17810f6
Sanmitha <sanmithasadanand@gmail.com>
2023-07-04 17:03:26 +0530

    Create Album Dashboard

--- client/src/components/DashboardAlbums.jsx
+++ client/src/components/DashboardAlbums.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { getAllAlbums } from '../api';
+import { actionType } from '../context/reducer';
+import SongCard from './SongCard';
+import { useStateValue } from '../context/StateProvider';
 
 const DashboardAlbums = () => {
+  const [{ allAlbums }, dispatch] = useStateValue();
+  useEffect(() => {
+    if(!allAlbums){
+      getAllAlbums().then ((data) => {
+        dispatch({
+          type: actionType.SET_ALL_ALBUMS,
+          allAlbums: data.album,
+        })
+      })
+    }
+  }, []);
+
   return (
-    <div>DashboardAlbums</div>
+    <div className="w-full p-4 flex items-center justify-center flex-col">
+        {/* Main container */}
+        <div className="relative w-full  my-4 p-4 
+        py-16 border border-gray-300 rounded-md">
+          <AlbumContainer  data={allAlbums}/>
+
+      </div>
+    </div>
   )
 }
+
+export const AlbumContainer = ({ data }) => {
+  return (
+    <div className=" w-full flex flex-wrap gap-3  items-center justify-evenly">
+      {data &&
+        data.map((song, i) => (
+          <SongCard
+          key={song._id} data={song} index={i} 
+          />
+        ))}
+    </div>
+  );
+};
 
 export default DashboardAlbums
