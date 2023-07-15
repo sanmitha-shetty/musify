5be0afb648218e05bbd0ab1e6df36ae3631cd48d
Sanmitha <sanmithasadanand@gmail.com>
2023-07-15 13:23:43 +0530

    Update index.js

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = "http://localhost:4000/";
+const baseURL = process.env.REACT_APP_BASE_URL;
 
 // Validate Users
 export const validateUser = async (token) => {
